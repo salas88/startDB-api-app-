603de3510ed833b418a838eca78fee1469b0c7ac
Vladislav <salas88>
2020-04-02 01:15:52 +0300

    add ItemList component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../app-header';
 import RandomPlanet from '../random-planet';
+import ItemList from '../item-list';
 import './app.css';
 
 const App = () => {
@@ -8,6 +9,15 @@ const App = () => {
         <div>
             <Header />
             <RandomPlanet />
+
+            <div className="row mb2">
+                <div className="col-md-6">
+                    <ItemList />
+                </div>
+                <div className="col-md-6">
+
+                </div>
+            </div>
         </div>
     )
 }
